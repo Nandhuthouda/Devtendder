bc6a401f974476219fa6419e67640406021a75d0
NANDHU THOUDA <nandhuthouda@gmail.com>
2025-06-27 21:54:54 +0530

    insert

--- src/app.js
+++ src/app.js
@@ -4,6 +4,23 @@ const connectDB  = require("./config/database");
 
 const app = express();
 
+const User = require("./models/users")
+app.post("/signup", (req, res) => {
+  try{
+    const user = new User ({
+    firstName : "Nandhu",
+    lastName : "thouda"
+  });
+  // Save the user to the database
+  user.save();
+    res.status(201).send("User created successfully");
+
+}catch(err){
+    console.error("Error saving user:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 
 connectDB()
 .then(() => {
